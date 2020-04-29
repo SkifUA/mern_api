96e2c7b4a5241c53f393e7fed43199cc6b39d271
Valerii Kuzin <vkuzin@zaraffasoft.com>
2020-04-29 16:32:35 +0300

    #5 Added Controllers

--- routes/places-rputes.js
+++ routes/places-rputes.js
@@ -2,53 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-const HttpError = require('../models/http-error')
+const placesController = require('../controllers/places-controller')
 
-const DUMMY_PLACES = [
-  {
-    id: 'p1',
-    title: 'Empire State Building',
-    description: 'One of the most famous sky scrapers of the world',
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg',
-    address: '20 W 34th St, New York, NY',
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584
-    },
-    creator: 'u1'
-  },
-  {
-    id: 'p2',
-    title: 'Empire State Building',
-    description: 'One of the most famous sky scrapers of the world',
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg',
-    address: '20 W 34th St, New York, NY',
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584
-    },
-    creator: 'u2'
-  }
-];
+router.get('/:pid', placesController.getPlaceById);
 
-router.get('/:pid', (req, res, next) => {
-  const placesId = req.params.pid;
-  const place = DUMMY_PLACES.find(p => p.id === placesId);
-
-  if (!place) {
-    throw new HttpError('Could not find a place', 404);
-  }
-  res.json({place});
-});
-
-router.get('/user/:uid', (req, res, next) => {
-  const userId = req.params.uid;
-  const place = DUMMY_PLACES.find(p => p.creator === userId);
-  if (!place) {
-    return next(new HttpError('Could not find a place', 404));
-  }
-
-  res.json({place})
-});
+router.get('/user/:uid', placesController.getPlaceByUserId);
 
 module.exports = router;
