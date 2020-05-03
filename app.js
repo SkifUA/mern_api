87ba3c7f10002d2428cc4a8bef1feb4af53c4da1
Valerii Kuzin <vkuzin@zaraffasoft.com>
2020-05-03 13:15:31 +0300

    #14 Implemented Mongoose

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -9,7 +10,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/api/places', placesRoutes);
-app.use('/api/users', usersRoutes)
+app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
   const error = new HttpError('Could not find this route.', 404);
@@ -21,8 +22,13 @@ app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
-  res.status(error.code || 500)
-  res.json({message: error.message || 'An unknown error occurred'})
+  res.status(error.code || 500);
+  res.json({message: error.message || 'An unknown error occurred'});
 });
 
-app.listen(5000);
+mongoose
+  .connect('mongodb+srv://manu:Pxswy4LZcLEUIdcF@cluster0-tqkab.mongodb.net/places?retryWrites=true&w=majority')
+  .then(() => app.listen(5000))
+  .catch( err => {
+    console.log(err);
+  });
